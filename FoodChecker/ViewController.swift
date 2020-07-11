2e145f1629cb1bb4381c31e70f2b5aeadb0cbc65
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-07-11 16:37:59 +0900

    Initial Commit

---
+++ FoodChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodChecker
+//
+//  Created by 福島大稀 on 2020/07/11.
+//  Copyright © 2020 Maresuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
